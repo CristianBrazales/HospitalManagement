16a877a2c73f25f9df6f0bb0c3e39f0038d54a20
Rodrigo-Rosillo <51249653+Rodrigo-Rosillo@users.noreply.github.com>
2019-06-02 15:25:03 -0700

    Doctor Draft

--- my-app/src/pages/Doctor.js
+++ my-app/src/pages/Doctor.js
@@ -1,39 +1,35 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import AddUnavailableDate from './AddUnavailableDate';
 import RemoveUnavailableDate from './RemoveUnavailableDate';
-import SignUpDoctor from './RegisterDoctor';
-import SignUpPatient from './RegisterPatient';
 
 class Doctor extends Component {
 
     render() {
         return (
-            <div className="App">
-                <div className="App__Aside">
-                    <div className="Receptionist_AppointmentsList">
-                        <label className="FormTitle__Link--ListPatients ">
-                            Upcoming Appointments</label>
-                    </div>
-                </div>
-
-                <div className="App__Form">
-                        <div className="FormTitle">
-                            <NavLink to="/Doctor/RegisterDoctor" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">
-                                Register Doctor</NavLink>or
-                            <NavLink exact to="/Doctor/RegisterPatient" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">
-                                Register Patient</NavLink>
+            <Router>
+                <div className="App">
+                    <div className="App__Aside">
+                        <div className="Receptionist_AppointmentsList">
+                            <label className="FormTitle__Link--ListPatients ">
+                                Upcoming Appointments</label>
                         </div>
+                    </div>
 
-
-                        <Route exact path="/Doctor/RegisterPatient" component={SignUpPatient}>
+                    <div className="App__Form">
+                        <div className="FormTitle">
+                            <NavLink to="/Doctor/AddUnavailableDate" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">
+                                Add Unavailable Date</NavLink>or
+                            <NavLink exact to="/Doctor/RemoveUnavailableDate" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">
+                                Remove Unavailable Date</NavLink>
+                        </div>
+                        <Route exact path="/Doctor/AddUnavailableDate" component={AddUnavailableDate}>
                         </Route >
-                        <Route exact path="/Doctor/RegisterDoctor" component={SignUpDoctor}>
+                        <Route exact path="/Doctor/RemoveUnavailableDate" component={RemoveUnavailableDate}>
                         </Route>
                     </div>
-
-            </div>
-
+                </div>
+            </Router>
         );
     }
 }
--- my-app/src/pages/Patient.js
+++ my-app/src/pages/Patient.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
-
 
 class Patient extends Component {
 
