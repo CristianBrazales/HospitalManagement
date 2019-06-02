08bf4fa9e986aa1491165caf17f8b91ad4236939
Rodrigo-Rosillo <51249653+Rodrigo-Rosillo@users.noreply.github.com>
2019-06-01 21:37:06 -0700

    Updates

---
+++ precache-manifest.7d6cca23ce54d406d157db29a555eca9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deedd239a353090f1c6c6d9f050ebd6a",
+    "url": "/HospitalManagement/index.html"
+  },
+  {
+    "revision": "c237c805ce7c3e777c7d",
+    "url": "/HospitalManagement/static/css/main.36363625.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/HospitalManagement/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "c237c805ce7c3e777c7d",
+    "url": "/HospitalManagement/static/js/main.8b740256.chunk.js"
+  },
+  {
+    "revision": "d47b0dab60a9b0349b4d",
+    "url": "/HospitalManagement/static/js/runtime~main.f0451e4d.js"
+  }
+]);
